d5fef874eebdfd25f084858d9fa473c87df2a2e8
Angel Torres <alas4224@gmail.com>
2019-03-06 08:09:24 -0600

    add test to dashboard component to test if strike, ball, foul, and hit buttons render

--- baseball-score-tracker/src/components/Dashboard.spec.js
+++ baseball-score-tracker/src/components/Dashboard.spec.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
-import Dashboard from './Display';
+import Dashboard from './Dashboard';
+import Display from './Display';
 
 describe('<Dashboard/>', () => {
     it('Should display strike button', () => {
@@ -15,5 +16,17 @@ describe('<Dashboard/>', () => {
         const { getByText } = render(<Dashboard/>)
 
         expect(getByText(/ball/i)).toBeInTheDocument();
+    })
+
+    it('Should display foul button', () => {
+        const { getByText } = render(<Dashboard/>)
+
+        expect(getByText(/foul/i)).toBeInTheDocument();
+    })
+
+    it('Should display hit button', () => {
+        const { getByText } = render(<Dashboard/>)
+
+        expect(getByText(/hit/i)).toBeInTheDocument();
     })
 })
